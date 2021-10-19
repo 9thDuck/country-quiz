ce37a3627277d008662a7f9c7169f24f62a38c1d
lumia 520 <msai@live.co.uk>
2021-10-19 12:08:25 +0530

    mobile setup remaining

--- src/App.js
+++ src/App.js
@@ -1,117 +1,176 @@
 import React, { useState, useEffect } from "react";
-import Loading from "./Loading";
 import { data } from "./data"; // replaced the fetch functionality with local data module
-import Question from "./Question"; // I have console loggged the state values in this module, please take a look at them.
+import Question from "./Question";
+import { v4 as uuid } from "uuid";
+import { SvgWinners, SvgAdventure } from "./Svg";
 
 const App = () => {
   ////////////////////////////////////////////////////////////////////////////
   //////////////////////////  STATES  ////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////
-  const [loading, setLoading] = useState(true);
-  const [allCountries, setAllCountries] = useState(data);
+
+  const [allCountries] = useState(data);
   const [selectedCountries, setSelectedCountries] = useState([]);
-  const [options, setOptions] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctAnswerCount, setCorrectAnswerCount] = useState(0);
 
   ////////////////////////////////////////////////////////////////////////////
   //////////////////////////  FUNCTIONS  /////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////
-
   const makeQuestions = () => {
-    const questionsTemp = [];
-    selectedCountries.forEach((country, index) => {
-      const randomNumber = Math.round(Math.random() * 4);
-      const { name, capital } = country;
-      console.log(country);
+    const questionsTemp = selectedCountries.map((country, index) => {
+      const randomNumber = Math.round(Math.random() * 3);
+      let options;
+      const { name, capital, callingCodes, region } = country;
+      let question;
       switch (randomNumber) {
-        case 1:
-          optionsList("name");
-          questionsTemp.push({
+        case 0:
+          options = optionsList(name, "name");
+          question = {
             question: `${capital} is the capital of...`,
             options,
+            answer: name,
+          };
+          options = undefined;
+          return question;
+
+        case 1:
+          options = optionsList(callingCodes, "callingCodes");
+          question = {
+            question: `Which is the calling code of ${name}?`,
+            options,
+            answer: callingCodes.flat(),
             index,
-          });
-          setOptions([]);
-          break;
+          };
+          options = undefined;
+          return question;
+
         case 2:
-          optionsList("callingCodes");
-          questionsTemp.push({
-            question: `Which is the calling code of ${name}`,
+          options = optionsList(capital, "capital");
+          question = {
+            question: `Which is the capital of ${name}?`,
             options,
+            answer: capital,
             index,
-          });
-          setOptions([]);
-          break;
+          };
+          options = undefined;
+          return question;
+
         case 3:
-          optionsList("name");
-          questionsTemp.push({
-            question: `Which country's capital is ${capital}?`,
-            options,
-            index,
-          });
-          setOptions([]);
-          break;
-        case 4:
-          optionsList("region");
-          questionsTemp.push({
+          options = optionsList(region, "region");
+          question = {
             question: `${name} is situated in...`,
             options,
-            index,
-          });
-          setOptions([]);
-          break;
+            answer: region,
+          };
+
+          options = undefined;
+          return question;
       }
     });
 
     setQuestions(questionsTemp);
   };
 
-  const optionsList = (param) => {
-    const optionsTemp = [];
-    while (optionsTemp.length < 4) {
+  const optionsList = (answer, param) => {
+    if (answer.typeOf === "array") {
+      answer = answer.flat();
+    }
+    const randomOptionPusher = () => {
       const randomIndex = Math.floor(Math.random() * allCountries.length);
       const optionObj = allCountries[randomIndex];
-      optionObj[param] === "callingCodes"
-        ? optionsTemp.push(...optionObj[param])
-        : optionsTemp.push(optionObj[param]);
+      let option;
+
+      option =
+        optionObj[param].typeOf === Array
+          ? optionObj[param].flat()
+          : optionObj[param];
+      if (option.length && optionsTemp.indexOf(option) === -1) {
+        optionsTemp.push(option);
+      }
+    };
+
+    let optionsTemp = [answer];
+    while (optionsTemp.length < 4) {
+      randomOptionPusher();
     }
-    setOptions(optionsTemp);
+    optionsTemp = optionsTemp.sort((a, b) => 0.5 - Math.random());
+    return optionsTemp;
   };
 
+  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////  useEffects /////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     const selectedCountriesTemp = [];
     while (selectedCountriesTemp.length < 20) {
-      const randomIndex = Math.floor(Math.random() * data.length);
-      const country = allCountries[randomIndex];
-      selectedCountriesTemp.push(country);
+      const randomIndex = Math.floor(Math.random() * allCountries.length);
+      selectedCountriesTemp.push(allCountries[randomIndex]);
     }
-
     setSelectedCountries(selectedCountriesTemp);
-    makeQuestions();
-    setLoading(false);
   }, []);
 
-  const questionChanger = () => {
-    console.log(currentQuestion);
-  };
+  useEffect(() => {
+    makeQuestions();
+  }, [selectedCountries]);
 
   ////////////////////////////////////////////////////////////////////////////
   //////////////////////////  RETURNS ////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////
 
-  if (loading) return <Loading />;
-
   return (
     <main className="app-main">
-      <section className="title">
-        <h1 className="title">Country Quiz</h1>
+      <section>
+        {questions.map((qustion, index) => {
+          const { question, options, answer } = qustion;
 
-        <Question
-          selectedCountries={selectedCountries}
-          currentQuestion={currentQuestion}
-          questions={questions}
-        />
+          return (
+            <div
+              className={`question-card-container ${
+                index === currentQuestion ? "active " : ""
+              }${index < currentQuestion ? "finished " : ""}${
+                index > currentQuestion ? "upcoming" : ""
+              }`}
+            >
+              <Question
+                key={uuid()}
+                question={question}
+                options={options}
+                qNum={index + 1}
+                correctAnswerCount={correctAnswerCount}
+                setCorrectAnswerCount={setCorrectAnswerCount}
+                answer={answer}
+                currentQuestion={currentQuestion}
+                index={index}
+                setCurrentQuestion={setCurrentQuestion}
+              />
+            </div>
+          );
+        })}
+
+        {currentQuestion === 20 && (
+          <article className="card results-container">
+            <div className="results-img-container">
+              <SvgAdventure />
+            </div>
+            <div className="results-info">
+              <h1 className="results-title">Results</h1>
+              <p className="info">
+                You got
+                <span className="score-span"> {correctAnswerCount} </span>
+                correct answers
+              </p>
+            </div>
+
+            <button
+              className="try-again-btn"
+              onClick={() => window.location.reload(true)}
+            >
+              Try again
+            </button>
+          </article>
+        )}
       </section>
     </main>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,10 +1,81 @@
-import React from "react";
+import React, { useRef } from "react";
+import { v4 as uuid } from "uuid";
+import { SvgAdventure } from "./Svg";
 
-const Question = ({ questions, currentQuestion,selectedCountries }) => {
+const Question = ({
+  question,
+  options,
+  answer,
+  currentQuestion,
+  setCorrectAnswerCount,
+  setCurrentQuestion,
+}) => {
+  const bullets = ["A", "B", "C", "D"];
+  const optionsContainer = useRef(null);
+  let btnClickCount = 0;
+
+  const evaluator = (e, option) => {
+    const btnClicked = e.currentTarget;
+    if (btnClickCount === 1) {
+      if (typeof answer === "object") {
+        answer = answer[0].toString();
+        option = option[0].toString();
+      } else {
+        answer = answer.toString();
+        option = option.toString();
+      }
+
+      Array.from(optionsContainer.current.children).forEach((btn) => {
+        if (btn.textContent.slice(1) === answer) {
+          btn.classList.add("right");
+        }
+      });
+
+      if (option === answer) {
+        setTimeout(() => {
+          setCorrectAnswerCount(
+            (correctAnswserCount) => correctAnswserCount + 1
+          );
+          setCurrentQuestion(currentQuestion + 1);
+        }, 1000);
+      } else {
+        Array.from(optionsContainer.current.children).forEach((btn) => btn);
+        btnClicked.classList.add("wrong");
+        setTimeout(() => {
+          setCurrentQuestion(currentQuestion + 1);
+        }, 1000);
+      }
+    }
+  };
+
   return (
-    <article className="quiz-card">
-      {console.log(selectedCountries, questions, currentQuestion)}
-      {questions && questions.map((question) => question.question)}
+    <article className="card question-card">
+      <h1 className="title">Country Quiz</h1>
+      <SvgAdventure />
+
+      <div className="question-container">
+        <h2>{question}</h2>
+      </div>
+      <div className="options-container" ref={optionsContainer}>
+        {options.map((option, index) => (
+          <button
+            onClick={(e) => {
+              btnClickCount += 1;
+              evaluator(e, option);
+            }}
+            className="option-btn"
+            key={uuid()}
+          >
+            {bullets[index]}
+            <div className="space"></div>
+            {option.length
+              ? option.length > 33
+                ? option.substr(0, 30) + "..."
+                : option
+              : "None of the above"}
+          </button>
+        ))}
+      </div>
     </article>
   );
 };
